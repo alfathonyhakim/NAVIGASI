06eaa5b35b5204437cb34f852bf172df48a1275b
alfatonyhakim <alfathony.hakim@yahoo.com>
2022-07-21 22:48:16 +0700

    content Teams sudah, untuk bug2 di akhir selesaikannya

--- scriptku.js
+++ scriptku.js
@@ -6,12 +6,59 @@ $(document).ready(function(){
             $('.navigasibar').removeClass("wush");
         }
     });
+
+    
     // kita beralih (togle) ke menu atau navigasi Script
     $('.tombol-mnu').click(function(){
         $('.navigasibar .menu').toggleClass("aktif");
         $('.tombol-mnu i').toggleClass("aktif");
     });
 });
+
+// kuda script
+$('.kudawks').owlCarousel({
+    margin: 20,
+    loop: true,
+    autoplayTimeOut: 2000,
+    autoplayHoverPause: true,
+    responsive: {
+        0:{
+            items: 1,
+            nav: false
+        },
+        600:{
+            items: 2,
+            nav: false
+        },
+        1000:{
+            items: 3,
+            nav: false
+        }
+
+    }
+});
+
+// $(document).ready(function(){
+//     $(window).scroll(function(){
+//         if(this.scrollY > 20){
+//             $('.title').addClass("ngumpet");
+//         }else{
+//             $('.title').removeClass("ngumpet");
+//         }
+//     });
+// });
+
+
+
+
+
+
+
+
+
+
+
+
 
 // ini js untuk belajar adik
 // const nama = "fiki";
